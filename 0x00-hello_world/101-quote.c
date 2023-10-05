b373ae2d33dc4f8ae39757170df454ebbb537ef4
IfeneticTech <inehpaul@gmail.com>
2023-10-05 11:17:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  *
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fprintf(stderr, "%s", str);
-	return (1);
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", str);
+return (1);
 }
