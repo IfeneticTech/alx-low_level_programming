926eb81b160c978e97dcdf6049a6d3ec05196692
IfeneticTech <inehpaul@gmail.com>
2023-10-10 06:11:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("_purchar\n");
+
+	return (0);
+}
