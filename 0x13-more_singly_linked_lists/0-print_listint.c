85eb68971ad5f308cedc464672c16fd5dceaef0e
IfeneticTech <inehpaul@gmail.com>
2023-11-20 09:57:24 -0800

    the mandatory tasks for the 0x13. C - More singly linked lists project

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,20 @@
 #include "lists.h"
 
 /**
- * print_listint - print the int data in a singly linked list
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
  *
- * @h: the head pointer to the list
- * Return: The number of nodes in the list
+ * Return: numbers of nodes.
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
+	size_t nnodes = 0;
 
-	if (h == NULL)
-		return (0);
-	for (count = 0; h != NULL; count++)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
