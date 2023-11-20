c02ff518628d6f5498a331de505d850f97c8fbfa
IfeneticTech <inehpaul@gmail.com>
2023-11-20 09:28:34 -0800

    the mandatory tasks for the 0x13. C - More singly linked lists project

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nodes;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	for (nodes = 0; h != NULL; nodes++)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
