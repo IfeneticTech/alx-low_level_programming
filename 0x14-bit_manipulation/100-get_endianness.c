65176727192bcbef384f8d9c1223b32ce5e71d92
IfeneticTech <inehpaul@gmail.com>
2023-11-23 05:51:40 -0800

    the advanced tasks for 0x14 bit manipulation project

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - check the system Byte order
+ *
+ * Return: 0 if big endian and 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int y = 1;
+	int little_or_big;
+
+	little_or_big = (int) (((char *)&y)[0]);
+	return (little_or_big);
+}
