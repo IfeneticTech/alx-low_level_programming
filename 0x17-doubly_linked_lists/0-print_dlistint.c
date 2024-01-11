779b7d810bbc3535989f0022c3f7b297c413f8b0
IfeneticTech <inehpaul@gmail.com>
2024-01-11 05:37:04 -0800

    Task 0 of the 0x17-doubly_linked_lists project

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - ifunction that prints all the elements of a
+ * print_dlistint - function that prints all the elements of a
  * dlistint_t list
  *
  * @h: head of the list
