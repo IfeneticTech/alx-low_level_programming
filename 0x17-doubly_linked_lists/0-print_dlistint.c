36499050e2c42b48ae354d7567d039b9a5abe77c
IfeneticTech <inehpaul@gmail.com>
2024-01-11 05:31:54 -0800

    Task 0 of the 0x17-doubly_linked_lists project

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - ifunction that prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int thecount;
+
+	thecount = 0;
+
+	if (h == NULL)
+		return (thecount);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		thecount++;
+		h = h->next;
+	}
+
+	return (thecount);
+}
